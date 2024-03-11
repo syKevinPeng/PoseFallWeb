ef60c4d930c738a76f6998d7edc938a461220306
Siyuan Peng @ WS <peng2000@umd.edu>
2024-03-10 20:29:45 -0400

    upd

---
+++ src/Animation.tsx
@@ -0,0 +1,264 @@
+import * as THREE from "three";
+
+import { createEffect, createSignal, onCleanup } from "solid-js";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
+import Stats from "three/examples/jsm/libs/stats.module";
+import { GUI } from "dat.gui";
+
+export const [activeAction, setActiveAction] =
+  createSignal<THREE.AnimationAction>();
+
+export function Animation(props: {
+  name: string;
+  model: string;
+  anims?: string[];
+}) {
+  let statsRef: HTMLDivElement | undefined;
+  let guiRef: HTMLDivElement | undefined;
+  let rendererRef: HTMLDivElement | undefined;
+  const [guiInfoS, setGuiInfo] = createSignal<{
+    gui: GUI;
+    modelsFolder: GUI;
+    animationsFolder: GUI;
+  }>();
+
+  //
+  //
+  //
+  const stats = new Stats();
+  stats.dom.style.position = "absolute";
+  createEffect(() => {
+    statsRef?.replaceChildren(stats.dom);
+  });
+
+  //
+  //
+  //
+  createEffect(() => {
+    props.name; // do not remove
+
+    const gui = new GUI({ autoPlace: false });
+    const modelsFolder = gui.addFolder("Models");
+    const animationsFolder = gui.addFolder("Animations");
+    setGuiInfo({ gui, modelsFolder, animationsFolder });
+
+    modelsFolder.open();
+    animationsFolder.open();
+    gui.domElement.style.position = "absolute";
+    gui.domElement.style.right = "0";
+
+    guiRef?.replaceChildren(gui.domElement);
+  });
+
+  //
+  //
+  //
+  createEffect(() => {
+    if (rendererRef == null) return;
+    console.log(`Loading ${props.name}!`);
+
+    const lastObjectsInScene: THREE.Object3D[] = [];
+    const lastAnimActions: THREE.AnimationAction[] = [];
+    let modelReady = false;
+
+    let mixer: THREE.AnimationMixer | undefined;
+    let animationCallbacks: { [key: string]: () => void } = {};
+    let lastAction: THREE.AnimationAction | undefined;
+
+    const scene = new THREE.Scene();
+    scene.add(new THREE.AxesHelper(10));
+
+    const light = new THREE.PointLight(0xffffff, 500);
+    light.position.set(2.5, 7.5, 15);
+    scene.add(light);
+
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    camera.position.set(0.8, 1.4, 1.0);
+
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setClearColor(0x404040); // Set background color to darker gray
+    onWindowResize(false);
+    rendererRef.replaceChildren(renderer.domElement);
+
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.enableDamping = true;
+    controls.target.set(0, 1, 0);
+
+    window.addEventListener("resize", () => onWindowResize(true), false);
+    function onWindowResize(shouldRender: boolean) {
+      const w = Math.min(
+        rendererRef?.clientWidth ?? Infinity,
+        document.body.clientWidth
+      );
+      const h = Math.min(
+        rendererRef?.clientHeight ?? Infinity,
+        document.body.clientHeight
+      );
+      console.log("w", w, "h", h);
+
+      camera.aspect = w / h;
+      camera.updateProjectionMatrix();
+
+      renderer.setSize(w, h);
+
+      if (shouldRender) render();
+    }
+
+    addModelAnim(props);
+
+    const clock = new THREE.Clock();
+    function animate() {
+      requestAnimationFrame(animate);
+      if (!modelReady) {
+        // throw new Error("Model not ready!!!!!");
+        return;
+      }
+
+      controls.update();
+
+      if (mixer != null) mixer.update(clock.getDelta());
+
+      render();
+
+      stats.update();
+    }
+
+    animate();
+
+    function render() {
+      renderer.render(scene, camera);
+    }
+
+    function addModelAnim({
+      name,
+      model,
+      anims,
+    }: {
+      name: string;
+      model: string;
+      anims?: string[];
+    }) {
+      // gui.destroy();
+      // gui = new GUI();
+
+      lastObjectsInScene.forEach((x) => {
+        scene.remove(x);
+      });
+      lastObjectsInScene.length = 0;
+
+      mixer?.uncacheRoot(mixer.getRoot());
+      mixer?.stopAllAction();
+      lastAnimActions.forEach((x) => {
+        x.stop();
+        mixer?.uncacheClip(x.getClip());
+        mixer?.uncacheAction(x.getClip());
+      });
+      animationCallbacks = {};
+
+      const fbxLoader: FBXLoader = new FBXLoader();
+
+      fbxLoader.load(
+        model,
+        (object) => {
+          if (!(object instanceof THREE.Object3D)) return;
+
+          object.scale.set(0.01, 0.01, 0.01);
+          mixer = new THREE.AnimationMixer(object);
+
+          object.animations.map((animation, i) => {
+            if (mixer == null) {
+              alert("Mixer not ready!!!!!");
+              throw new Error("Mixer not ready!!!!!");
+            }
+            const animationAction = mixer.clipAction(animation);
+            lastAnimActions.push(animationAction);
+
+            const propname = name + (i != 0 ? ` ${i + 1}` : "");
+            animationCallbacks[propname] = function () {
+              setAction(animationAction);
+            };
+            guiInfoS()?.modelsFolder.add(animationCallbacks, propname);
+            // if (i === 0 || lastAction == null) setActiveAction(animationActions[0]);
+          });
+
+          lastObjectsInScene.push(object);
+          scene.add(object);
+
+          for (const anim of anims ?? [])
+            fbxLoader.load(
+              anim,
+              (object) => {
+                if (!(object instanceof THREE.Object3D)) return;
+
+                object.animations.map((animation, i) => {
+                  if (mixer == null) {
+                    alert("Mixer not ready!!!!!");
+                    throw new Error("Mixer not ready!!!!!");
+                  }
+                  const animationAction = mixer.clipAction(animation);
+                  lastAnimActions.push(animationAction);
+
+                  const propname =
+                    `${name} Animation` + (i != 0 ? ` ${i + 1}` : "");
+                  animationCallbacks[propname] = function () {
+                    setAction(animationAction);
+                  };
+                  guiInfoS()?.animationsFolder.add(
+                    animationCallbacks,
+                    propname
+                  );
+                });
+
+                lastObjectsInScene.push(object);
+              },
+              (xhr) => {
+                //   console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+              },
+              console.error
+            );
+
+          modelReady = true;
+        },
+        (xhr) => {
+          //   console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+        },
+        console.error
+      );
+    }
+
+    function setAction(toAction: THREE.AnimationAction) {
+      if (!modelReady) {
+        alert("Model not ready!!!!!");
+        throw new Error("Model not ready!!!!!");
+      }
+
+      if (toAction != activeAction()) {
+        console.log(
+          `setAction: to=${toAction} active=${activeAction()} last=${lastAction}`
+        );
+        lastAction = activeAction();
+        setActiveAction(toAction);
+        lastAction?.fadeOut(1);
+        activeAction()?.reset();
+        activeAction()?.fadeIn(1);
+        activeAction()?.play();
+        lastAction?.stop();
+      }
+    }
+  });
+
+  return (
+    <div style={{ flex: 1, display: "flex", position: "relative" }}>
+      <div ref={statsRef} style={{ position: "relative" }} />
+      <div ref={guiRef}  />
+      <div ref={rendererRef} style={{ flex: 1 }} />
+    </div>
+  );
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,134 @@
-import * as THREE from "three";
-import { onMount, onCleanup, createSignal, createEffect } from "solid-js";
-import { animation } from "./animation";
-import { GUI } from "dat.gui";
+import { createSignal, createEffect, Show } from "solid-js";
 
-const gui = new GUI();
-export const [activeAction, setActiveAction] =
-  createSignal<THREE.AnimationAction>();
+import { Animation, activeAction } from "./Animation";
+
+const modelAnims = [
+  {
+    name: "SMPL Male",
+    model: "fbx_files/male_model.fbx",
+    anims: ["fbx_files/test_animation.fbx"],
+  },
+  {
+    name: "SMPL Female",
+    model: "fbx_files/female_model.fbx",
+    anims: ["fbx_files/test_animation.fbx"],
+  },
+];
 
 export function App() {
   const [time, setTime] = createSignal(0);
   const [duration, setDuration] = createSignal(0);
-
-  onMount(() => animation(gui, activeAction, setActiveAction));
-  onCleanup(() => {
-    gui.destroy();
-    document.getElementById("threejs")!.replaceChildren();
-  });
+  const [modelAnim, setModelAnim] = createSignal(modelAnims[0]);
 
   createEffect(() => {
-    setDuration(Math.ceil(activeAction()?.getClip().duration || 0));
-    let stop = false;
-    function updTime() {
-      requestAnimationFrame(updTime);
-      setTime(activeAction()?.time || 0);
+    const dur = activeAction()?.getClip().duration;
+    if (dur != null && dur > 1) {
+      setDuration(dur);
+
+      let stop = false;
+      function updTime() {
+        if (stop || duration() === 0) return;
+        requestAnimationFrame(updTime);
+        // console.log(
+        //   "activeAction()?.time",
+        //   activeAction()?.time,
+        //   activeAction()?.getClip().duration
+        // );
+        setTime(activeAction()?.time || 0);
+      }
+      updTime();
+
+      return () => {
+        stop = true;
+      };
+      //
+      //
+    } else {
+      setDuration(0);
+      setTime(0);
     }
-    (async () => updTime())();
-    return () => {
-      stop = true;
-    };
   });
 
   return (
-    <div style={{ display: "flex", "justify-content": "center" }}>
-      <pre style={{ width: "5rem" }}>
-        {(Math.round(time() * 100) / 100).toFixed(2)}/{duration()}
-      </pre>
-      <input
-        type="range"
-        min="0"
-        max={duration()}
-        step="0.01"
-        value={time()}
-        onInput={(e) => {
-          console.log("e.currentTarget.value", e.currentTarget.value);
-          const _activeAction = activeAction();
-          if (_activeAction) _activeAction.time = +e.currentTarget.value;
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        "flex-direction": "column",
+        "overflow-y": "hidden",
+      }}
+    >
+      <div style={{ display: "flex", "justify-content": "center" }}>
+        {modelAnims.map((x) => (
+          <button
+            onClick={() => {
+              setModelAnim(x);
+            }}
+          >
+            {x.name}
+          </button>
+        ))}
+      </div>
+      <div
+        style={{
+          position: "fixed",
+          bottom: "10%",
+          width: "100%",
+          display: "flex",
+          "justify-content": "center",
+          "z-index": 100,
         }}
-      />
+      >
+        <div
+          style={{
+            width: "40%",
+            display: "flex",
+            margin: "auto",
+            "padding-left": "2rem",
+            "padding-right": "2rem",
+            "column-gap": ".3rem",
+            "background-color": "rgba(0, 0, 0, 0.2)",
+          }}
+        >
+          <pre
+            style={{
+              display: "inline-block",
+              "flex-basis": "3rem",
+              color: "white",
+            }}
+          >
+            {(Math.round(time() * 100) / 100).toFixed(2)}/
+            {(Math.round(duration() * 100) / 100).toFixed(2)}
+          </pre>
+          <input
+            type="range"
+            min="0"
+            max={duration()}
+            step="0.01"
+            style={{ "flex-grow": "1" }}
+            value={time()}
+            onInput={(e) => {
+              console.log("e.currentTarget.value", e.currentTarget.value);
+              const _activeAction = activeAction();
+              if (_activeAction) _activeAction.time = +e.currentTarget.value;
+            }}
+          />
+        </div>
+      </div>
+      <Animation {...modelAnim()} />
+      {/* 
+      
+  addModelAnim({
+    name: "SMPL Male",
+    model: "fbx_files/male_model.fbx",
+    anims: ["fbx_files/test_animation.fbx"],
+  });
+  addModelAnim({
+    name: "SMPL Female",
+    model: "fbx_files/female_model.fbx",
+    anims: ["fbx_files/test_animation.fbx"],
+  });
+   */}
     </div>
   );
 }
